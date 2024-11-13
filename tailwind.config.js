82030d8c2eff6be32522cca61f4381db93802bc9
ChrisVai <chrisbellet@gmail.com>
2024-11-13 11:57:31 +0100

    Tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      'primaryFontColor': '#262626',
+
+      'veryLightGrey': '#D9D9D9',
+      'lightGrey': '#8C8C8C',
+      'grey': '#595959',
+      'darkGrey': '#262626',
+      'black': '#0D0D0D',
+      'darkBlue': '#010326',
+      'blue': '#07B0F2',
+      'green': '#0FA650',
+      'yellow': '#F2B138',
+      'orange': '#F25922',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
