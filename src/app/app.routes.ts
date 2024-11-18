ae1f67e2962635a5cce820f514ab69f3bee916a6
ChrisVai <chrisbellet@gmail.com>
2024-11-18 16:53:15 +0100

    update routes = > auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth-component/auth.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+];
