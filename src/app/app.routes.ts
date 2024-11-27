588da573734341abbd183e27315ff36b974b000e
ChrisVai <chrisbellet@gmail.com>
2024-11-27 14:26:26 +0100

    route for Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth-component/auth.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'auth',
     component: AuthComponent,
