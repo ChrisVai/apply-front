74f107bc9796ae8f0aa718c712bb54347a39e0e2
ChrisVai <chrisbellet@gmail.com>
2024-11-27 14:27:42 +0100

    login method

--- src/app/features/auth/service/auth.service.ts
+++ src/app/features/auth/service/auth.service.ts
@@ -8,30 +8,44 @@ import {
 } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment.development';
-import { UserModel } from '../../../shared/models/userModel';
 import { Observable, tap } from 'rxjs';
+import { UserModel } from '../../../shared/models/userModel';
+import { LoginResponseModel } from '../../../shared/models/loginResponseModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly _http: HttpClient = inject(HttpClient);
-  private _currentUser: WritableSignal<UserModel | null> =
-    signal<UserModel | null>(null);
+  private _user: WritableSignal<UserModel | null> = signal<UserModel | null>(
+    null
+  );
   private readonly _apiUrl = environment.apiUrl;
-  currentUser: Signal<UserModel | null> = this._currentUser.asReadonly();
-  isConnected: Signal<Boolean> = computed(() => this.currentUser !== null);
 
-  login(email: string, pass: string): Observable<UserModel> {
+  user: Signal<UserModel | null> = this._user.asReadonly();
+  accessToken: WritableSignal<String | null> = signal<String | null>(null);
+  refreshToken: WritableSignal<String | null> = signal<String | null>(null);
+  isConnected: Signal<Boolean> = computed(() => this.user !== null);
+
+  login(email: string, pass: string): Observable<LoginResponseModel> {
     return this._http
-      .post<UserModel>(
+      .post<LoginResponseModel>(
         this._apiUrl + '/auth/login',
-        { email, pass },
+        {
+          email: email,
+          password: pass,
+        },
         { withCredentials: true }
       )
       .pipe(
         tap(response => {
-          this._currentUser.set(response);
+          this._user.set(response.user);
+          this.accessToken.set(response.tokens.access_token);
+          this.refreshToken.set(response.tokens.refresh_token);
+          //todo à virer debug
+          console.log(response);
+          console.log(response.user);
+          console.log(response.tokens);
         })
       );
   }
