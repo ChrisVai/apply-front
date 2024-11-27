0283e693beaeea2ab449a1cf19de1f5e068c28d1
ChrisVai <chrisbellet@gmail.com>
2024-11-27 14:27:24 +0100

    Creating form to login

--- src/app/features/auth/auth-component/auth.component.ts
+++ src/app/features/auth/auth-component/auth.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  private readonly _fb: FormBuilder = inject(FormBuilder);
+  private readonly _authService: AuthService = inject(AuthService);
+  private readonly _destroyRef: DestroyRef = inject(DestroyRef);
+  private readonly _router: Router = inject(Router);
+  errorMessage: string | undefined;
+
+  authForm = this._fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  login() {
+    const val = this.authForm.value;
+    if (val.email && val.password) {
+      this._authService
+        .login(val.email, val.password)
+        .pipe(takeUntilDestroyed(this._destroyRef))
+        .subscribe({
+          next: () => this._router.navigateByUrl('/home'),
+          error: () =>
+            (this.errorMessage = 'Identifiants invalides, veuillez réessayer'),
+        });
+    }
+  }
+}
