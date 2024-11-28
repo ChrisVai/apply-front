470af37d8bc23ef58645b726a3bf6da69b4d4e18
ChrisVai <chrisbellet@gmail.com>
2024-11-28 10:31:29 +0100

    update loginResponseModel = no longer need to refreshToken attribute

--- src/app/shared/models/loginResponseModel.ts
+++ src/app/shared/models/loginResponseModel.ts
@@ -1,9 +1,6 @@
 import { UserModel } from './userModel';
 
 export interface LoginResponseModel {
-  tokens: {
-    access_token: string;
-    refresh_token: string;
-  };
+  access_token: string;
   currentUser: UserModel;
 }
