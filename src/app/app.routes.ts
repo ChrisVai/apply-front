619b6570cdc35e970c768093c849d5c1bf6edf88
ChrisVai <chrisbellet@gmail.com>
2024-12-11 10:35:24 +0100

    Update routes : empty path, Authguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth-component/auth.component';
 import { HomeComponent } from './features/home/home.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
     path: 'auth',
     component: AuthComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard],
+    pathMatch: 'full',
   },
 ];
