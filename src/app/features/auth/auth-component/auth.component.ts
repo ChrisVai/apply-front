2729efcfe560705ab6294c0715c253d1532195ce
ChrisVai <chrisbellet@gmail.com>
2024-12-11 17:07:54 +0100

    update auth component : input validation behaviors

--- src/app/features/auth/auth-component/auth.component.ts
+++ src/app/features/auth/auth-component/auth.component.ts
@@ -1,14 +1,15 @@
-import { Component, DestroyRef, inject } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { Component, computed, DestroyRef, inject, Signal } from '@angular/core';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../service/authService/auth.service';
 import { Router } from '@angular/router';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [NgOptimizedImage, ReactiveFormsModule],
+  imports: [NgOptimizedImage, ReactiveFormsModule, NgClass],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
@@ -17,12 +18,40 @@ export class AuthComponent {
   private readonly _authService: AuthService = inject(AuthService);
   private readonly _destroyRef: DestroyRef = inject(DestroyRef);
   private readonly _router: Router = inject(Router);
-  errorMessage: string | undefined;
 
+  errorMessage: string | undefined;
   authForm = this._fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
+
+  isInvalidMail: Signal<boolean | undefined> = toSignal(
+    this.authForm.controls.email.statusChanges.pipe(
+      map(
+        () =>
+          this.authForm.controls.email.invalid &&
+          (this.authForm.controls.email.dirty ||
+            this.authForm.controls.email.touched)
+      )
+    )
+  );
+  isInvalidPassword: Signal<boolean | undefined> = toSignal(
+    this.authForm.controls.password.statusChanges.pipe(
+      map(
+        () =>
+          this.authForm.controls.password.invalid &&
+          (this.authForm.controls.password.dirty ||
+            this.authForm.controls.password.touched)
+      )
+    )
+  );
+  isInvalidForm: Signal<boolean> = computed(
+    () =>
+      this.isInvalidPassword() ||
+      this.isInvalidPassword() === undefined ||
+      this.isInvalidMail() ||
+      this.isInvalidMail() === undefined
+  );
 
   login() {
     const val = this.authForm.value;
