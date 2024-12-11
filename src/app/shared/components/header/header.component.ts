f6b2c0875d17dd1bc2d7c2a1b8acf740e36a7237
ChrisVai <chrisbellet@gmail.com>
2024-12-11 10:37:07 +0100

    update Header : logout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, Signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { AuthService } from '../../../features/auth/service/authService/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,12 @@ import { ButtonComponent } from '../button/button.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly authService: AuthService = inject(AuthService);
+  isConnected: Signal<boolean> = this.authService.isConnected;
+
+  logout() {
+    console.log('passage dans logout header');
+    this.authService.logout();
+  }
+}
