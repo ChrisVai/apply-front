0f86c47944befb649cba6b56aefa94e135fcd704
ChrisVai <chrisbellet@gmail.com>
2024-12-16 17:02:40 +0100

    adding createUser method

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment.development';
+import { UserModel } from '../../models/userModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private readonly _handler: HttpBackend = inject(HttpBackend);
+  //Use _httpWithHandler to bypass Interceptor when we don't want to add access_token to the request
+  private readonly _httpWithHandler: HttpClient = new HttpClient(this._handler);
+  private readonly _http: HttpClient = inject(HttpClient);
+  private readonly _userApiUrl: string = environment.apiUrl + '/users';
 
-  constructor() { }
+  createUser(user: UserModel) {
+    return this._httpWithHandler.post(this._userApiUrl, {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: user.password,
+    });
+  }
 }
