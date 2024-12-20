062a4fc4b39e2db48497a58f0127bde0d409814d
ChrisVai <chrisbellet@gmail.com>
2024-12-20 16:21:37 +0100

    auto-Refresh Companies when added

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, Signal } from '@angular/core';
 import { AddApplicationComponent } from './add-application/add-application.component';
 import { AddCompanyComponent } from './add-company/add-company.component';
 import { MyApplicationsComponent } from './my-applications/my-applications.component';
-import { ApplicationService } from '../../shared/services/application/application.service';
 import { CompanyService } from '../../shared/services/company/company.service';
 import { CompanyModel } from '../../shared/models/companyModel';
 import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
@@ -20,18 +19,16 @@ import { toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  private readonly _applicationService: ApplicationService =
-    inject(ApplicationService);
   private readonly _companyService: CompanyService = inject(CompanyService);
-  private refreshTrigger$ = new BehaviorSubject<void>(undefined);
-
-  allCompanies$: Observable<CompanyModel[]> = this.refreshTrigger$.pipe(
-    switchMap(() => this._companyService.getAllCompanies())
-  );
+  private _refreshCompaniesTrigger$: BehaviorSubject<void> =
+    new BehaviorSubject<void>(undefined);
+  allCompanies$: Observable<CompanyModel[]> =
+    this._refreshCompaniesTrigger$.pipe(
+      switchMap(() => this._companyService.getAllCompanies())
+    );
   allCompaniesSignal: Signal<CompanyModel[]> = toSignal(this.allCompanies$, {
     initialValue: [],
   });
-
   showAddCompanyForm: boolean = false;
 
   showAddCompanyFormEvent($event: boolean) {
@@ -39,7 +36,7 @@ export class HomeComponent {
   }
 
   //used to trigger the refresh of allCompanies$
-  refresh() {
-    this.refreshTrigger$.next();
+  refreshCompanies() {
+    this._refreshCompaniesTrigger$.next();
   }
 }
