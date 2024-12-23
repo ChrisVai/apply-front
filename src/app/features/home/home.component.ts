1b7d9231fb769492f7f1c378097d0fccc23091f4
ChrisVai <chrisbellet@gmail.com>
2024-12-23 19:52:36 +0100

    reverse application's array to basic display

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,7 +4,7 @@ import { AddCompanyComponent } from './add-company/add-company.component';
 import { MyApplicationsComponent } from './my-applications/my-applications.component';
 import { CompanyService } from '../../shared/services/company/company.service';
 import { CompanyModel } from '../../shared/models/companyModel';
-import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, map, Observable, switchMap, tap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ApplicationService } from '../../shared/services/application/application.service';
 import { ApplicationModel } from '../../shared/models/applicationModel';
@@ -53,7 +53,7 @@ export class HomeComponent {
   });
   myApplicationsSignal: Signal<ApplicationModel[]> = toSignal(
     //todo consolelog à virer debug
-    this.myApplications$.pipe(tap(data => console.log(data))),
+    this.myApplications$.pipe(map(data => data.reverse())),
     { initialValue: [] }
   );
   /*
