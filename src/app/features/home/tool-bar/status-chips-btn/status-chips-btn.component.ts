5af2b91121694ffbf70297cc6300d5362f36122d
ChrisVai <chrisbellet@gmail.com>
2025-01-08 14:53:19 +0100

    Add status chips btn

--- src/app/features/home/tool-bar/status-chips-btn/status-chips-btn.component.ts
+++ src/app/features/home/tool-bar/status-chips-btn/status-chips-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, InputSignal, OnInit } from '@angular/core';
+import { Component, Input, input, InputSignal } from '@angular/core';
 
 @Component({
   selector: 'app-status-chips-btn',
@@ -7,13 +7,7 @@ import { Component, Input, input, InputSignal, OnInit } from '@angular/core';
   templateUrl: './status-chips-btn.component.html',
   styleUrl: './status-chips-btn.component.scss',
 })
-export class StatusChipsBtnComponent implements OnInit {
-  @Input() color: string = '';
+export class StatusChipsBtnComponent {
   @Input({ required: true }) name!: string;
   count: InputSignal<number> = input.required<number>();
-  outlineColorClass: string = '';
-
-  ngOnInit(): void {
-    this.outlineColorClass = `outline-app-${this.color}`;
-  }
 }
