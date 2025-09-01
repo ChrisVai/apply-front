3fbddf00a61789add9330bbb5bcb7051bc352125
ChrisVai <chrisbellet@gmail.com>
2025-09-01 12:09:39 +0200

    moving application input to InputSignal
    used to handle asynchronous refresh of applications Status text color

--- src/app/features/home/my-applications/application-card/application-card.component.ts
+++ src/app/features/home/my-applications/application-card/application-card.component.ts
@@ -1,17 +1,14 @@
 import {
   Component,
+  computed,
   DestroyRef,
   inject,
   input,
-  Input,
   InputSignal,
-  OnChanges,
-  OnInit,
   output,
   OutputEmitterRef,
   Signal,
   signal,
-  SimpleChanges,
   WritableSignal,
 } from '@angular/core';
 import {
@@ -22,12 +19,7 @@ import { ApplicationService } from '../../../../shared/services/application/appl
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { DatePipe } from '@angular/common';
 import { EditApplicationComponent } from './edit-application/edit-application.component';
-import {
-  FormBuilder,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-application-card',
@@ -41,8 +33,9 @@ import {
   templateUrl: './application-card.component.html',
   styleUrl: './application-card.component.scss',
 })
-export class ApplicationCardComponent implements OnInit {
-  @Input({ required: true }) application!: ApplicationModel;
+export class ApplicationCardComponent {
+  application: InputSignal<ApplicationModel> =
+    input.required<ApplicationModel>();
   refreshApplicationsOutput: OutputEmitterRef<void> = output<void>();
 
   private readonly _applicationService: ApplicationService =
@@ -50,10 +43,24 @@ export class ApplicationCardComponent implements OnInit {
   private readonly _destroyRef: DestroyRef = inject(DestroyRef);
 
   applicationDeleted: WritableSignal<boolean> = signal<boolean>(false);
+  statusClass: Signal<string> = computed(() => {
+    switch (this.application().status) {
+      case Status.applied:
+        return 'text-app-green-400';
+      case Status.toApply:
+      case Status.toRelaunch:
+        return 'text-app-yellow-400';
+      case Status.relaunched:
+        return 'text-app-green-400';
+      case Status.closed:
+        return 'text-app-red-400';
+      default:
+        return 'text-app-blue-400';
+    }
+  });
 
   showComments: boolean = false;
   showEditor: boolean = false;
-  statusClass: string = '';
 
   spreadApplicationModifiedEvent() {
     this.refreshApplicationsOutput.emit();
@@ -76,27 +83,5 @@ export class ApplicationCardComponent implements OnInit {
         error: () => console.error('application non supprimée'),
         complete: () => console.log('application supprimée'),
       });
-  }
-
-  ngOnInit(): void {
-    switch (this.application.status) {
-      case Status.applied:
-        this.statusClass = 'text-app-green-400';
-        break;
-      case Status.toApply:
-        this.statusClass = 'text-app-yellow-400';
-        break;
-      case Status.toRelaunch:
-        this.statusClass = 'text-app-yellow-400';
-        break;
-      case Status.relaunched:
-        this.statusClass = 'text-app-green-400';
-        break;
-      case Status.closed:
-        this.statusClass = 'text-app-red-400';
-        break;
-      default:
-        this.statusClass = 'text-app-blue-400';
-    }
   }
 }
